b4fb1545712d2f50d5c5477fad035484c280a73a
kansd1401 <kang.sanjeet1401@gmail.com>
2019-10-24 23:32:02 +0000

    added ip fucntionality

---
+++ iss.js
@@ -0,0 +1,19 @@
+const request = require('request');
+
+const fetchMyIP = (callback) => {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    callback(error,JSON.parse(body)['ip']);
+  });
+};
+
+
+module.exports = { fetchMyIP };
